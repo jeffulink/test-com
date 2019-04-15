98fb1b0ef3e1304717934d560a05375c133b2b84
Ammar Ashfaq <ammar_ashfaq@yahoo.com>
2019-04-15 04:05:24 -0700

    build

---
+++ build/precache-manifest.97f55396a5fb8ea4cbd3340b183fed26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12ef746c62d9610fe2096efce60bcfe8",
+    "url": "/static/media/background.12ef746c.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4fffef2626a798f1b799",
+    "url": "/static/js/main.4fffef26.chunk.js"
+  },
+  {
+    "revision": "cb41768e36de55678309",
+    "url": "/static/js/2.cb41768e.chunk.js"
+  },
+  {
+    "revision": "4fffef2626a798f1b799",
+    "url": "/static/css/main.020348d7.chunk.css"
+  },
+  {
+    "revision": "05034f5ac1c3a913dfd98d12f9c557d7",
+    "url": "/index.html"
+  }
+];
